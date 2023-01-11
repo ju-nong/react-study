48e5849fd3611c615c1b23e85104a8cf0d4aa3be
이준용 <iiiuooyoo@gnewsoft.com>
2023-01-11 16:21:46 +0900

    useEffect쪽 봐주셈!

--- my-app2/src/UserList.js
+++ my-app2/src/UserList.js
@@ -1,28 +1,21 @@
-import React, { useRef } from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle }) {
-    // const $el = useRef();
-
-    // const activeUser = () => {
-    //     $el.current.classList.toggle("activate");
-    // };
-
-    // return (
-    //     <div ref={$el} onClick={activeUser} style={{ cursor: "pointer" }}>
-    //         <b>
-    //             {user.username} <span>({user.email})</span>
-    //         </b>
-    //         <button onClick={() => onRemove(user.id)}>삭제</button>
-    //     </div>
-    // );
+    // 왜 show hide show 되냐고
+    useEffect(() => {
+        console.log("Show");
+        return () => {
+            console.log("Hide");
+        };
+    }, []);
 
     return (
-        <div
-            className={user.active ? "activate" : ""}
-            style={{ cursor: "pointer" }}
-            onClick={() => onToggle(user.id)}
-        >
-            <b>
+        <div>
+            <b
+                className={user.active ? "activate" : ""}
+                style={{ cursor: "pointer" }}
+                onClick={() => onToggle(user.id)}
+            >
                 {user.username} <span>({user.email})</span>
             </b>
             <button onClick={() => onRemove(user.id)}>삭제</button>
