cbe704a2d702cf1fc6af5cbed260ae0b8434056f
이준용 <iiiuooyoo@gmail.com>
2023-01-27 13:51:35 +0900

    refact : 쓸모 없는 파일 삭제

--- guidebook-todo/src/App.js
+++ guidebook-todo/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { createGlobalStyle } from "styled-components";
 import { TodoTemplate } from "./components/TodoTemplate";
 import { TodoHead } from "./components/TodoHead";
