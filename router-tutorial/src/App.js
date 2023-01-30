c3983cee41ac38829304c9b799d9b7ccc1481b29
이준용 <iiiuooyoo@gmail.com>
2023-01-30 15:16:08 +0900

    feat : react-router-dom 실습

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { Home, About } from "./Introduce";
+
+function App() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">홈</Link>
+                </li>
+                <li>
+                    <Link to="/about">소개</Link>
+                </li>
+            </ul>
+            <hr />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
